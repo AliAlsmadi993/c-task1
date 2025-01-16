0ae24046918dbfa4038bfff57e27d8b28cdc9bc9
AliAlsmadi993 <alsmadiali993@gmail.com>
2025-01-16 16:17:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 - Create console application that read string from user and print the same string
+
+
+            //Console.WriteLine("enter your name");
+            // string name=Console.ReadLine();
+            //Console.WriteLine("welcome" + " " + name);
+
+            //22-	Define variables with kind of
+
+            int num = 5;
+            double number = 5.11;
+            string name = "ali";
+            char x = 'A';
+            bool a = true;
+            const int j = 10;
+            Console.WriteLine( num+" "+number+" "+name+" "+x+" "+a+" "+j);
+
+
+        }
+    }
+}
