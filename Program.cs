64b819f96e2e2423547b8fcde388658274ab6504
AliAlsmadi993 <alsmadiali993@gmail.com>
2025-01-19 16:50:58 +0300

    task2

--- Program.cs
+++ Program.cs
@@ -12,22 +12,28 @@ namespace task1
         static void Main(string[] args)
         {
             // Task 1: Create console application that reads string from user and prints it
-            /*
-            Console.WriteLine("enter your name");
-            string name = Console.ReadLine();
-            Console.WriteLine("welcome" + " " + name);
-            */
+
+            //Console.WriteLine("enter your name");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("welcome" + " " + name);
+
+
+
 
             // Task 2: Define variables and print them
-            /*
-            int num = 5;
-            double number = 5.11;
-            string name = "ali";
-            char x = 'A';
-            bool a = true;
-            const int j = 10;
-            Console.WriteLine(num + " " + number + " " + name + " " + x + " " + a + " " + j);
-            */
+            
+            //int num = 5;
+            //double number = 5.11;
+            //string name = "ali";
+            //char x = 'A';
+            //bool a = true;
+            //const int j = 10;
+            //Console.WriteLine(num + " " + number + " " + name + " " + x + " " + a + " " + j);
+            
+
+
+
+
 
             // Task 3: Define array with cars and print them
             /*
@@ -38,8 +44,11 @@ namespace task1
             }
             */
 
+
+
+
             // Task 4: Collect user details and print them
-            /*
+            
             Console.WriteLine("enter your first name");
             string firstName = Console.ReadLine();
 
@@ -50,7 +59,11 @@ namespace task1
             int age = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine(firstName + " " + lastName + " " + age);
-            */
+            
+
+
+
+
 
             // Task 5: Input array elements and print them
 
@@ -75,5 +88,5 @@ namespace task1
 
 
         }
-}
+    }
 }
